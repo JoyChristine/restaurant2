2765654e37133ff30034d1bd47a8dbb0786af487
Joy Christine <joychristin2@gmail.com>
2020-11-10 22:09:49 +0300

    initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import ' ../App.css'
+// const Contact = () => {
+//     return ()
+// }export default Contact;
+import React from 'react'
+import '../App.css'
+const Contact = () => {
+    return (
+        <div></div>
+
+    )
+}
+export default Contact;
---
+++ src/components/Menu.js
@@ -0,0 +1,5 @@
+// import React from 'react';
+// import ' ../App.css'
+// const Menu = () => {
+//     return ()
+// }export default Menu;
