a4715bd9052b39c84dc73b3a4db08a4127b49a34
Joy Christine <joychristin2@gmail.com>
2020-11-11 13:33:35 +0300

    module edited

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,29 @@ import React from 'react'
 import '../App.css'
 const Contact = () => {
     return (
-        <div></div>
-
+        
+     <div class="contact">
+    <h2>Contact Us</h2>
+    <h3> Call us on 123-456-789 or Write to us via our email:chocolatelyhse@gmail.com</h3>
+    <ul class="contact-items">
+        <li class="contact-item">
+            <a href="https://www.facebook.com/" class="networks-list__item-link">
+                <i class="fa fa-facebook-square" aria-hidden="true">Facebook</i>
+            </a>
+        </li>
+        <li class="contact-item">
+            <a href="https://www.instagram.com/" class="networks-list__item-link">
+                <i class="fa fa-instagram" aria-hidden="true">instagram</i>
+            </a>
+        </li>
+        <li class="contact-item">
+            <a href="https://www.twitter.com/" class="networks-list__item-link">
+                <i class="fa fa-twitter" aria-hidden="true">Twitter</i>
+            </a>
+        </li>
+    </ul>
+</div>
+    
     )
 }
 export default Contact;
