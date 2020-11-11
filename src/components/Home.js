9343c343320b839ea8416709fe14b47849ecf2c2
Joy Christine <joychristin2@gmail.com>
2020-11-11 13:39:33 +0300

    Added text

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ' ../App.css'
 const Home = () => {
     return (
-        <div></div>
+       <div class="home"> 
+       <h2>Best Dessert in Town</h2>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus malesuada leo eget metus pretium fermentum.Etiam ultrices, quam sed fermentum maximus, lacus ex vestibulum urna, eu suscipit erat nisl eu quam.Ut at condimentum quam.Nullam
+        eu ullamcorper nunc, ac dapibus enim.Nunc dignissim ullamcorper mi vitae blandit.Curabitur eget condimentum lacus, sit amet dictum mauris.Fusce ultrices enim erat, in ullamcorper risus aliquet ut.Morbi ac lectus et mi varius porta.
+        Phasellus ut mi tristique, feugiat arcu vel, venenatis justo Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus malesuada leo eget metus pretium fermentum.Etiam ultrices, quam sed fermentum maximus, lacus ex vestibulum  </p>
+        </div> 
+
     )
 }
 export default Home;
