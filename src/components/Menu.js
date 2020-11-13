5c4bb271d864b2abf76642219fd635d5a0b44a76
Joy Christine <joychristin2@gmail.com>
2020-11-13 21:47:21 +0300

    corrected error on importing css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import ' ../App.css'
-const Menu = () => {
+import css from '../App.css'
+import angelFoodCake from '../components/img/angelFoodCake.jpg'
+
+const Menu = ()=>{
     return (
 
 <div class="menu" id="menu">
-    <div class="dessert"> 
-         <img src="img/angelFoodCake.jpg" alt=""></img>
+    <div className="dessert"> 
+         <img src={angelFoodCake} alt="hhh"/>
         <p>Angel food cake, or angel cake, is a type of sponge cake made with egg whites, flour, and sugar. A whipping agent, such as cream of tartar, is commonly added. It differs from other cakes because it uses no butter. Its aerated texture comes
             from whipped egg white. Angel food cake originated in the United States and first became popular in the late 19th century. It gained its unique reputation along with its name due to its light and fluffy texture.
         </p> 
     </div> 
 
-    <div class="dessert"> 
-        <img src="img/angelFoodCake.jpg" alt=""></img>
+    <div className="dessert"> 
+    <img src={angelFoodCake} alt=""/>
         <p>Angel food cake, or angel cake, is a type of sponge cake made with egg whites, flour, and sugar. A whipping agent, such as cream of tartar, is commonly added. It differs from other cakes because it uses no butter. Its aerated texture comes
             from whipped egg white. Angel food cake originated in the United States and first became popular in the late 19th century. It gained its unique reputation along with its name due to its light and fluffy texture.
         </p>
 
     </div>
     <div class="dessert">
-    <img src="img/angelFoodCake.jpg" alt=""></img>
+    <img src={angelFoodCake} alt=""/>
         <p>Angel food cake, or angel cake, is a type of sponge cake made with egg whites, flour, and sugar. A whipping agent, such as cream of tartar, is commonly added. It differs from other cakes because it uses no butter. Its aerated texture comes
             from whipped egg white. Angel food cake originated in the United States and first became popular in the late 19th century. It gained its unique reputation along with its name due to its light and fluffy texture.</p>
     </div>
     <div class="dessert">
-    <img src="img/angelFoodCake.jpg" alt=""></img>
+    <img src={angelFoodCake} alt="cake"></img>
         <p>Angel food cake, or angel cake, is a type of sponge cake made with egg whites, flour, and sugar. A whipping agent, such as cream of tartar, is commonly added. It differs from other cakes because it uses no butter. Its aerated texture comes
             from whipped egg white. Angel food cake originated in the United States and first became popular in the late 19th century. It gained its unique reputation along with its name due to its light and fluffy texture.</p>
     </div>
