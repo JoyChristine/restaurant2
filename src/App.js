4ca02c6829f16ee9b3ec75ade87e8f92dc660932
Joy Christine <joychristin2@gmail.com>
2020-11-13 15:05:03 +0300

    added navbar, switch not functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 // import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom";
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import './App.css'
+import Navbar from './components/Navbar';
+import menu from './components/Menu';
+import home from './components/Home';
+import contact from './components/Contact';
 
-from
+
 
 function App() {
-    return ( <></>
+    return (  
+    <>
+        <Navbar /> 
+        <Router>
+     <Switch>
+        <Route exact patch ='/' component={home} /> 
+        <Route exact patch ='/menu' component={menu} />
+        <Route exact patch ='/contact' component={contact} />
+    </Switch> 
+    </Router>
+
+    </>
     );
 
 }
